8562ce45861b970297a6a0275677374c8aac5bce
Oliver Köhler <oliver@acolono.com>
2017-02-26 13:49:45 +0100

    Initial commit.

--- config-sounds.dist.js
+++ config-sounds.dist.js
@@ -8,15 +8,7 @@ var aliases = {
     ':poop:': './tracks/smileys/hankey.mp3',
     ':shit:': './tracks/smileys/hankey.mp3',
     // Example for for custom sounds.
-    'stresserei': './tracks/stressbot4.mp3',
-    'stress': './tracks/stressbot.mp3',
-    'wahnsinn': './tracks/wahnsinn1.mp3',
-    'gaudi': './tracks/gaudi.mp3',
-    'aaa': './tracks/WilhelmScream.mp3',
-    'uhoh': './tracks/uhoh.mp3',
-    'prost': './tracks/prost.mp3',
-    'badum': './tracks/BaDumTss.mp3',
-    'countdown': './tracks/countdown.mp3'
+    //'uhoh': './tracks/uhoh.mp3',
 };
 
 module.exports = aliases;
